cb3aa822758ead3e15b6586a69eef597a096dfbf
Ryan Kelley <rkelleyfx@gmail.com>
2020-01-25 14:14:41 -0800

    add key

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '54.86.240.232',
       username: 'ubuntu',
-      privateKey: 'C:\\Users\\Ryan\'s Tablet\\Downloads\\deploy-final-key.pem'
+      privateKey: './deploy-final-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
